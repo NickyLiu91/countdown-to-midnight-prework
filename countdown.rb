bbd87e8a548adbfae33617d80155249f4b1bff0f
Nicky Liu <nickyliu91@gmail.com>
2018-08-11 05:11:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,7 @@
 
 def countdown(integer)
   while integer != 0
+<<<<<<< HEAD
     puts "#{integer} SECOND(S)!"
     integer -= 1
   end
@@ -16,4 +17,10 @@ def countdown_with_sleep(integer)
     sleep 2
   end
   "HAPPY NEW YEAR!"
+=======
+    puts "#{integer} SECOND(S)"
+    integer -= 1
+  end
+  "HAPPY NEW YEAR!"
+>>>>>>> 3e8c46598111ff3de52a076de3996a7af8e50e28
 end
